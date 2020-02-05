61502544a3c81294d31b6394f0c1d9e1927e0b9a
Ioana Trandafir <ioana.stoica89@gmail.com>
2020-02-05 17:20:20 +0200

    removed parent folder

--- FinalProject/toast.js
+++
@@ -1,5 +0,0 @@
-function launch_toast() {
-    var x = document.getElementById("toast")
-    x.className = "show";
-    setTimeout(function() { x.className = x.className.replace("show", ""); }, 5000);
-}
